d6448049de14139ab135a7c7be33ea9c78958822
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-06-03 23:27:41 +0530

    added routes for votes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   root 'articles#index'
 
-  get '/articles', to: 'articles#index'
-
-  get 'articles/new', to: 'articles#new'
-
-  post '/articles', to: 'articles#create'
+  resources :articles, only: [:new, :create, :index] do
+    resources :votes, only: [:create]
+  end
 
   devise_for :users, path: '/', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: '' }
 end
