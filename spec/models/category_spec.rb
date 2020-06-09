d693da4f054be519a26e5da9649a1b3ce818b464
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-06-09 12:07:10 +0530

    added default scope test for category model

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -8,5 +8,14 @@ RSpec.describe Category, type: :model do
 
   describe "validations" do
     it { should validate_uniqueness_of(:name).case_insensitive }
-  end  
+  end
+
+  describe "default scope" do
+    let(:category1) { Category.create!(name: "Category1", priority: 3) }
+    let(:category1) { Category.create!(name: "Category2", priority: 4) }
+
+    it "should return categories ordered by priority DESC" do
+      expect(Category.all.to_sql).to eq(Category.order(priority: :desc).to_sql)
+    end    
+  end
 end
