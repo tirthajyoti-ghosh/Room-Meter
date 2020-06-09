5d7c99015089cede56b0cae3ceae02db3ef38206
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-06-09 21:18:09 +0530

    added tests for votes controller. added more tests for categories controller

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -5,8 +5,6 @@ RSpec.describe CategoriesController, type: :feature do
   context "categories controller actions" do
     let(:user) { User.create!(name: "Example User01", email: "example-1@user.com", password: "password", password_confirmation: "password") }
 
-    subject { Vote.create!(user_id: user.id, article_id: article.id) } 
-
     before(:each) do      
       @category = Category.create!(name: 'Anything', priority: 5)
 
@@ -29,6 +27,11 @@ RSpec.describe CategoriesController, type: :feature do
     scenario "category show page has the class 'author' and it contains the author name" do
       expect(page).to have_selector('.article-body .author')
       expect(page.find('.article-body .author').text).to eq("by  #{user.name}")
+    end
+
+    scenario "category show page has the class 'text' and it contains the article text" do
+      expect(page).to have_selector('.article-body .text')
+      expect(page.find('.article-body .text').text).to eq(@article.text)
     end
 
     scenario "category show page has the class 'vote' and it contains number of votes" do
---
+++ spec/controllers/votes_controller_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+require 'capybara/rspec'
+
+RSpec.describe VotesController, type: :feature do
+  context "votes controller actions" do
+    let(:user) { User.create!(name: "Example User01", email: "example-1@user.com", password: "password", password_confirmation: "password") }
+
+    before(:each) do      
+      @category = Category.create!(name: 'Anything', priority: 5)
+
+      @article = user.articles.create!(title: "Example Title01", text: "Example text01", image: "https://example.com", category_list: @category.name)
+
+      visit category_path(@category)
+      fill_in 'user_email', with: user.email
+      fill_in 'user_password', with: user.password
+      click_button 'Login'
+    end
+
+    scenario "one vote is created when user clicks link with class 'vote'" do
+      click_link '0'
+
+      expect(@article.votes.count).to eq(1)
+    end
+
+    scenario "vote link changes to p element when clicked(vote casted)" do
+      click_link '0'
+
+      expect(page).to have_selector('.article-body p.voted')
+      expect(page.find('.article-body p.voted').text).to eq("1")
+    end
+  end
+end
