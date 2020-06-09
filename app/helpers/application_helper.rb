d73e960fc9f4c36ba1cbbdc4a6f140e1a7a470c1
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-06-10 00:28:02 +0530

    added tests for helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,9 +12,9 @@ module ApplicationHelper
     end
   end
 
-  def display_vote_for(article)
+  def display_vote_for(article, user_id)
     vote_count = article.votes.count
-    unless already_voted?(article)
+    unless already_voted?(article, user_id)
       link_to(article_votes_path(article), method: :post,  class: "vote") do
         "<i class='far fa-heart'></i> #{vote_count}".html_safe
       end
@@ -25,7 +25,7 @@ module ApplicationHelper
     end
   end
 
-  def already_voted?(article)
-    Vote.exists?(user_id: current_user.id, article_id: article.id)
+  def already_voted?(article, user_id)
+    Vote.exists?(user_id: user_id, article_id: article.id)
   end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -11,8 +11,52 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe ApplicationHelper, type: :helper do
-  describe ApplicationHelper do
-    
-  end
+  let(:user) { User.create!(name: "Example User01", email: "example-1@user.com", password: "password", password_confirmation: "password") }
+
+  let(:user2) { User.create!(name: "Example User02", email: "example-2@user.com", password: "password", password_confirmation: "password") }
   
+  let(:category) { Category.create!(name: 'Anything', priority: 5) }
+  
+  let(:article1) { user.articles.create!(title: "Example Title01", text: "Example text01", image: "https://example.com", category_list: "Anything") }
+
+  let(:article2) { user.articles.create!(title: "Example Title02", text: "Example text02", image: "https://example.com", category_list: "category2") }
+
+  before do    
+    Vote.create!(user_id: user.id, article_id: article1.id)
+    Vote.create!(user_id: user2.id, article_id: article1.id)
+
+    Vote.create!(user_id: user.id, article_id: article2.id)
+  end
+
+  describe "vote_count" do
+    it "returns the vote count for the given article" do
+      expect(vote_count(article1.id)).to eq(2)
+    end      
+  end
+
+  describe "max_voted_article" do
+    it "returns the max voted article" do
+      expect(max_voted_article).to eq(article1)
+    end      
+  end
+
+  describe "already_voted?" do
+    it "returns false if given user has not voted for the given article" do
+      expect(already_voted?(article2, user2.id)).to eq(false)
+    end
+
+    it "returns true if given user has voted for the given article" do
+      expect(already_voted?(article2, user.id)).to eq(true)
+    end        
+  end
+
+  describe "display_vote_for" do
+    it "returns an a tag with class 'vote' when user has not already voted" do
+      expect(display_vote_for(article2, user2.id)).to match(/<a class="vote"/)
+    end
+    
+    it "returns a p tag with class 'voted' when user has already voted" do
+      expect(display_vote_for(article1, user2.id)).to match(/<p class="voted"/)
+    end
+  end  
 end
