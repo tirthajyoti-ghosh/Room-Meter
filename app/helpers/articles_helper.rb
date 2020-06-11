6a84f280aba3f535b202bc7152edf0ccae585ee7
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-06-11 15:56:44 +0530

    removed logic from categories#show view

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,24 @@
 module ArticlesHelper
+  def category_articles
+    html = ''
+    (0...@category.articles.length).each do |i|
+      html += "<div class='article-row'>" if i.even?
+      html += <<~MLS
+        <div class="article">
+          <img alt="#{@category.articles[i].title}" class="image" src="#{@category.articles[i].image}" width="200" height="150">
+          <article class="article-body">
+            <h2>#{@category.name}</h2>
+            <h3>#{@category.articles[i].title}</h3>
+            <p class="author">by &nbsp;<span class="name">#{@category.articles[i].author.name}</span></p>
+            <p class="text">#{@category.articles[i].text}</p>
+            #{display_vote_for(@category.articles[i], current_user.id)}
+          </article>
+        </div>
+      MLS
+
+      html += '</div>' if i.odd? || @category.articles.length == 1
+    end
+
+    html.html_safe
+  end
 end
