4d8a01118a64c3002aa4d199b9c0f46f23883540
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-06-11 09:17:36 +0530

    fixed navbar links styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  get 'categories/show'
   root 'articles#index'
 
   resources :articles, only: [:new, :create, :index] do
     resources :votes, only: [:create]
   end
 
-  resources :categories, only: [ :show]
+  resources :categories, only: [:show]
 
   devise_for :users, path: '/', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: '' }
 end
